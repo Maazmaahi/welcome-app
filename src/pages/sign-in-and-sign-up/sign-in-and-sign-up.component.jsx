d6d15ecb0ef2e979a03e0755cafa8525eb3810bb
maaz <nehalmaaz@gmail.com>
2020-10-21 16:41:38 +0530

    first commit

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.style.scss';
+
+import SignIn from '../../components/sign-in/Sign-in.component';
+import SignUp from '../../components/sign-up/Sign-up.component';
+
+const SignInAndSignUp = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+        <SignUp />
+    </div>
+);
+
+export default SignInAndSignUp;
